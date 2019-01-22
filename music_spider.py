e92b2bf37d771d842728d5723540bf3b41c89ab6
Your NamePeanuTxT <568112825@qq.com>
2019-01-22 23:37:35 +0800

    增加 爬取歌单详细信息

--- music_spider.py
+++ music_spider.py
@@ -10,37 +10,25 @@ import time
 函数功能：爬取网易云歌单信息
 参数：
     url：起始url
-    songsheets_list：歌单信息list
 返回：
     无
 '''
-def get_songsheet_url(url, songsheets_list):
-    global count
+def get_songsheet_url(url):
     global page
 
-    # 无页面浏览器配置
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-
     # selenium 请求url
-    driver = webdriver.Chrome(executable_path='C:/Users/zhouxy/AppData/Local/Google/Chrome/Application/chromedriver.exe', chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path='driver/chromedriver.exe', chrome_options=chrome_options)
     driver.get(url)
 
     # 切换到frame标签
     driver.switch_to.frame('g_iframe')
 
-    # 获取歌单信息
+    # 获取歌单link
     songsheets = driver.find_elements_by_xpath('//*[@id="m-pl-container"]/*')
     print('=====' * 10, f'page[{page}]', '=====' * 10)
     for songsheet in songsheets:
-        title = songsheet.find_element_by_xpath('div/a').get_attribute('title')
         link = songsheet.find_element_by_xpath('div/a').get_attribute('href')
-        play_times = int(songsheet.find_element_by_xpath('div/div').text.replace('万', '0000'))
-        creator = songsheet.find_element_by_xpath('p[2]/a').text
-        songsheets_list.append([title, link, play_times, creator])
-        print(f'[{count}] {title} {link} {play_times} {creator} done.')
-        count += 1
+        get_song_url(link)
 
     # 自动翻页
     next_page = driver.find_element_by_xpath('//*[@id="m-pl-pager"]/div/a[11]').get_attribute('href')
@@ -48,14 +36,48 @@ def get_songsheet_url(url, songsheets_list):
         driver.close()
         time.sleep(0.5)
         page += 1
-        get_songsheet_url(next_page, songsheets_list)
+        get_songsheet_url(next_page)
     # driver.close()
 
+def get_song_url(url):
+    global count
+
+    # selenium 请求url
+    driver = webdriver.Chrome(executable_path='driver/chromedriver.exe', chrome_options=chrome_options)
+    driver.get(url)
+    print(f'url:{url}')
+
+    # 切换到frame标签
+    driver.switch_to.frame('g_iframe')
+
+    # 歌单信息
+    title = driver.find_element_by_xpath('//div[@class="hd f-cb"]/div/h2').text
+    play_times = int(driver.find_element_by_xpath('//*[@id="play-count"]').text)
+    collection_times = int(driver.find_element_by_xpath('//*[@id="content-operation"]/a[3]/i').text.replace('万', '0000')[1:-1])
+    relay_times = int(driver.find_element_by_xpath('//*[@id="content-operation"]/a[4]/i').text[1:-1])
+    comments = int(driver.find_element_by_xpath('//*[@id="cnt_comment_count"]').text)
+    creator = driver.find_element_by_xpath('//div[@class="user f-cb"]/span/a').text
+    create_date = driver.find_element_by_xpath('//div[@class="user f-cb"]/span[2]').text.split(' ')[0]
+
+    songsheets_list.append([title, url, play_times, collection_times, relay_times, comments, creator, create_date])
+    print(f'[{count}] ', title, url, play_times, collection_times, relay_times, comments, creator, create_date, 'done.')
+    count += 1
+
+    driver.close()
+
+
 if __name__ == '__main__':
+
+    # 无页面浏览器配置
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+
     page = count = 1
     songsheets_list = []
-    get_songsheet_url('https://music.163.com/#/discover/playlist/?cat=%E6%97%A5%E8%AF%AD', songsheets_list)
-    songsheets_df = pd.DataFrame(songsheets_list, columns=['title', 'link', 'play_times', 'creator'])
-    songsheets_df.sort_values('play_times', ascending=False).to_csv(f'songsheets.csv', sep=',', na_rep='NA')
+    get_songsheet_url('https://music.163.com/#/discover/playlist/?cat=%E6%AC%A7%E7%BE%8E')
+    songsheets_df = pd.DataFrame(songsheets_list, columns=['title', 'play_times', 'link', 'collection_times',
+                                                           'relay_times', 'comments', 'creator', 'create_date'])
+    songsheets_df.sort_values('comments', ascending=False).to_csv(f'songsheets.csv', sep=',', na_rep='NA')
 
 
