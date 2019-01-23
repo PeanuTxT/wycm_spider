ad7753c2b4578d1e1b909ba1cc127adbe9c8af63
Peanut <568112825@qq.com>
2019-01-23 16:42:31 +0800

    修改部分代码

--- music_spider.py
+++ music_spider.py
@@ -4,6 +4,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import pandas as pd
 import time
+import json
+
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from fake_useragent import UserAgent
 
 
 '''
@@ -15,6 +19,7 @@ import time
 '''
 def get_songsheet_url(url):
     global page
+    global link_txt
 
     # selenium 请求url
     driver = webdriver.Chrome(executable_path='driver/chromedriver.exe', chrome_options=chrome_options)
@@ -28,7 +33,8 @@ def get_songsheet_url(url):
     print('=====' * 10, f'page[{page}]', '=====' * 10)
     for songsheet in songsheets:
         link = songsheet.find_element_by_xpath('div/a').get_attribute('href')
-        get_song_url(link)
+        link_txt += link + '\n'
+        # get_song_url(link)
 
     # 自动翻页
     next_page = driver.find_element_by_xpath('//*[@id="m-pl-pager"]/div/a[11]').get_attribute('href')
@@ -43,7 +49,11 @@ def get_song_url(url):
     global count
 
     # selenium 请求url
-    driver = webdriver.Chrome(executable_path='driver/chromedriver.exe', chrome_options=chrome_options)
+    # driver = webdriver.Chrome(executable_path='driver/chromedriver.exe', chrome_options=chrome_options)
+    driver = webdriver.Chrome(
+        executable_path='C:/Users/zhouxy/AppData/Local/Google/Chrome/Application/chromedriver.exe',
+        chrome_options=chrome_options)
+
     driver.get(url)
     print(f'url:{url}')
 
@@ -67,17 +77,32 @@ def get_song_url(url):
 
 
 if __name__ == '__main__':
-
+    # ip = json.loads(requests.get('http://daili.spbeen.com/get_api_json?token=d9RHl6r5iCCVgyVD9jR6fnSo&num=1').text)['data'][0]
+    # print(ip)
     # 无页面浏览器配置
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument(UserAgent().random)
+    chrome_options.add_argument(f'--proxy-server=http://119.101.116.127:9999')
+
+    desired_capabilities = DesiredCapabilities.CHROME  # 修改页面加载策略
+    desired_capabilities["pageLoadStrategy"] = "none"  # 注释这两行会导致最后输出结果的延迟，即等待页面加载完成再输出
 
     page = count = 1
-    songsheets_list = []
+    link_txt = ''
     get_songsheet_url('https://music.163.com/#/discover/playlist/?cat=%E6%AC%A7%E7%BE%8E')
-    songsheets_df = pd.DataFrame(songsheets_list, columns=['title', 'play_times', 'link', 'collection_times',
-                                                           'relay_times', 'comments', 'creator', 'create_date'])
-    songsheets_df.sort_values('comments', ascending=False).to_csv(f'songsheets.csv', sep=',', na_rep='NA')
+    # with open('link_list.txt', 'w') as f:
+    #     f.write(link_txt)
+
+    # songsheets_list = []
+    # with open('link_list.txt', 'r') as f:
+    #     for link in f.readlines():
+    #         get_song_url(link)
+    #         time.sleep(0.5)
+    #
+    # songsheets_df = pd.DataFrame(songsheets_list, columns=['title', 'play_times', 'link', 'collection_times',
+    #                                                        'relay_times', 'comments', 'creator', 'create_date'])
+    # songsheets_df.sort_values('comments', ascending=False).to_csv(f'songsheets.csv', sep=',', na_rep='NA')
 
 
