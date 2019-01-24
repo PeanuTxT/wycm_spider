2ace03ffd4aeb51999fe134a628bea5e85809833
Peanut <568112825@qq.com>
2019-01-24 15:40:00 +0800

    欧美歌单信息

---
+++ link_spider.py
@@ -0,0 +1,66 @@
+# -*- coding:utf-8 -*-
+
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+from selenium import webdriver
+import time
+
+
+'''
+函数功能：爬取网易云歌单url
+参数：
+    url：起始url
+返回：
+    无
+'''
+def get_songsheet_url(url):
+    global page
+    global link_txt
+
+    # selenium 请求url
+    driver.get(url)
+
+    # 切换到frame标签
+    driver.switch_to.frame('g_iframe')
+
+    # 获取歌单link
+    songsheets = driver.find_elements_by_xpath('//*[@id="m-pl-container"]/*')
+    print('=====' * 10, f'page[{page}]', '=====' * 10)
+    for songsheet in songsheets:
+        link = songsheet.find_element_by_xpath('div/a').get_attribute('href')
+        link_txt += link + '\n'
+
+    # 自动翻页
+    next_page = driver.find_element_by_xpath('//*[@id="m-pl-pager"]/div/a[11]').get_attribute('href')
+    if next_page != 'javascript:void(0)':
+        time.sleep(0.5)
+        page += 1
+        get_songsheet_url(next_page)
+
+
+if __name__ == '__main__':
+
+    # 无页面浏览器配置
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument(UserAgent().random)
+    chrome_options.add_argument(f'--proxy-server=http://113.231.199.122:4237')
+
+    # 修改selenium页面加载策略
+    desired_capabilities = DesiredCapabilities.CHROME
+    desired_capabilities["pageLoadStrategy"] = "none"
+
+    # selenium 启动无页面浏览器
+    driver = webdriver.Chrome(executable_path='driver/chromedriver.exe', chrome_options=chrome_options)
+
+    page = count = 1
+    link_txt = ''
+    get_songsheet_url('https://music.163.com/#/discover/playlist/?cat=%E6%AC%A7%E7%BE%8E')
+
+    # selenium 关闭无页面浏览器
+    driver.close()
+
+    with open('link_list1.txt', 'w') as f:
+        f.write(link_txt)
---
+++ songsheets_spider.py
@@ -0,0 +1,77 @@
+# -*- coding:utf-8 -*-
+
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+from selenium import webdriver
+import pandas as pd
+import time
+
+'''
+函数功能：爬取网易云歌单信息
+参数：
+    url：起始url
+返回：
+    无
+'''
+def get_songsheet_data(url):
+    global count
+
+    # selenium 请求url
+    driver.get(url)
+    print(f'url:{url}')
+
+    # 切换到frame标签
+    driver.switch_to.frame('g_iframe')
+
+    # 歌单信息
+    title = driver.find_element_by_xpath('//div[@class="hd f-cb"]/div/h2').text
+    play_times = int(driver.find_element_by_xpath('//*[@id="play-count"]').text)
+    collection_times = int(driver.find_element_by_xpath('//*[@id="content-operation"]/a[3]/i').text.replace('万', '0000')[1:-1])
+    if driver.find_element_by_xpath('//*[@id="content-operation"]/a[4]/i').text == '分享':
+        relay_times = 0
+    else:
+        relay_times = int(driver.find_element_by_xpath('//*[@id="content-operation"]/a[4]/i').text[1:-1])
+    if driver.find_element_by_xpath('//*[@id="cnt_comment_count"]').text == '评论':
+        comments = 0
+    else:
+        comments = int(driver.find_element_by_xpath('//*[@id="cnt_comment_count"]').text)
+    creator = driver.find_element_by_xpath('//div[@class="user f-cb"]/span/a').text
+    create_date = driver.find_element_by_xpath('//div[@class="user f-cb"]/span[2]').text.split(' ')[0]
+
+    songsheets_list.append([title, url, play_times, collection_times, relay_times, comments, creator, create_date])
+    print(f'[{count}] ', title, url, play_times, collection_times, relay_times, comments, creator, create_date, 'done.')
+    count += 1
+
+
+if __name__ == '__main__':
+
+    # 无页面浏览器配置
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument(UserAgent().random)
+    chrome_options.add_argument(f'--proxy-server=http://113.231.199.122:4237')
+
+    # 修改selenium页面加载策略
+    desired_capabilities = DesiredCapabilities.CHROME
+    desired_capabilities["pageLoadStrategy"] = "none"
+
+    # selenium 启动无页面浏览器
+    driver = webdriver.Chrome(executable_path='driver/chromedriver.exe', chrome_options=chrome_options)
+
+    page = count = 1
+    songsheets_list = []
+    with open('link_list.txt', 'r') as f:
+        for link in f.readlines():
+            get_songsheet_data(link)
+            time.sleep(0.5)
+
+    # selenium 关闭无页面浏览器
+    driver.close()
+
+    songsheets_df = pd.DataFrame(songsheets_list, columns=['title', 'link', 'play_times', 'collection_times',
+                                                           'relay_times', 'comments', 'creator', 'create_date'])
+    songsheets_df.sort_values('comments', ascending=False).to_csv(f'output/songsheets_12.csv', sep=',', na_rep='NA')
+
+
